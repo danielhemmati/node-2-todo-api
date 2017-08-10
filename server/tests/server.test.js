3f110f74b51f15e4b85ac5ae9ee433f90abd197e
unknown <danialups1997@gmail.com>
2017-08-11 00:07:12 +0430

    Add tests for GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -7,12 +7,20 @@ const {Todo} = require('./../models/todo.js');
 // there is a problem that if you run the test , you have something in your database so the line that says toB(0)
 // will fail so beacause of that , before we run the test case we should wipe the database (and really is going to delete //all of the db)
 
+//now we are going to insert some datas for GET route 
+const todos = [{
+    text:'first test todos'
+},{
+    text: 'second test todos'
+}];
 
 beforeEach(function(done){
     Todo.remove({}).then(function(){
-        done();
-    })
-})
+        return Todo.insertMany(todos);
+    }).then(function(){
+        done(); 
+    });
+});
 
 describe('POST /todos', function(){
     it('should create new todo', function(done){ // it's asynchronous so it shlould take done argument
@@ -30,15 +38,15 @@ describe('POST /todos', function(){
                 return done(err); // will print the error on the screen 
             }
 
-            Todo.find().then(function(todos){
+            Todo.find({text}).then(function(todos){
                 // the 1 is always correct because we always wipe the database first
                 expect(todos.length).toBe(1);// because there is one text that we pass in 
                 expect(todos[0].text).toBe(text);
                 done();
             }).catch(function(e){ // we use catch for any potential error
                 done(e);
-            })
-          })
+            });
+          });
            
     });
     
@@ -53,11 +61,24 @@ describe('POST /todos', function(){
             }
 
             Todo.find().then(function(todos){
-                expect(todos.length).toBe(0);
+                expect(todos.length).toBe(2);
                 done();
             }).catch(function(e){
                 done(e);
+            });
+        });
+    });
+});
+
+describe('GET /todos', function(){
+    // all of this todos is the todo we define above (line 11)
+    it('should get all todos', function(done){
+        request(app)
+            .get('/todos')
+            .expect(200)
+            .expect(function(res){
+                expect(res.body.todos.length).toBe(2)
             })
-        })
+            .end(done);
     })
 })
