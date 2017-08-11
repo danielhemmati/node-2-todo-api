e2f4d42a8092e8fa7956ef23b10575465cb25c48
unknown <danialups1997@gmail.com>
2017-08-12 00:13:13 +0430

    missing B character

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 // there is the history why they do this ... word
 mongoose.Promise = global.Promise;
 // mongodb://<dbuser>:<dbpassword>@ds055895.mlab.com:55895/danielapp
-var databaseURL = process.env.DATAASEURL;
+var databaseURL = process.env.DATABASEURL;
 mongoose.connect( databaseURL ||'mongodb://localhost:27017/TodoApp'); 
 
 module.exports = {
