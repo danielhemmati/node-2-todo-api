2ff85bac4c470b5588d7b857b774630514cff575
unknown <danialups1997@gmail.com>
2017-08-12 00:15:50 +0430

    testing for deploying .. just testing

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,6 +4,7 @@ var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 // mongodb://<dbuser>:<dbpassword>@ds055895.mlab.com:55895/danielapp
 var databaseURL = process.env.DATABASEURL;
+
 mongoose.connect( databaseURL ||'mongodb://localhost:27017/TodoApp'); 
 
 module.exports = {
