610a84bd80d1a11506348be705b6009f254392be
unknown <danialups1997@gmail.com>
2017-08-12 00:04:02 +0430

    export the url

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,8 +3,8 @@ var mongoose = require('mongoose');
 // there is the history why they do this ... word
 mongoose.Promise = global.Promise;
 // mongodb://<dbuser>:<dbpassword>@ds055895.mlab.com:55895/danielapp
-var databaseuri = process.env.DATABASEURI;
-mongoose.connect( databaseuri ||'mongodb://localhost:27017/TodoApp', {
+var databaseURL = process.env.DATAASEURL;
+mongoose.connect( databaseURL ||'mongodb://localhost:27017/TodoApp', {
     useMongoClient: true
 }); 
 
