a845005a5b8d7d02c27226c6257fff809f77a0c3
unknown <danialups1997@gmail.com>
2017-08-12 00:24:26 +0430

    remove mongoclient again

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,9 +4,7 @@ var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 // mongodb://<dbuser>:<dbpassword>@ds055895.mlab.com:55895/danielapp
 var databaseURL = process.env.DATABASEURL;
-mongoose.connect( databaseURL ||'mongodb://localhost:27017/TodoApp', {
-    useMongoClient: true
-}); 
+mongoose.connect( databaseURL ||'mongodb://localhost:27017/TodoApp'); 
 
 module.exports = {
     mongoose // as you know you can write it in different way but shorter the better 
