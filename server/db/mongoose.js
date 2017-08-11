661ac294b4ea12df75d803f8498f7a2e40f75e13
unknown <danialups1997@gmail.com>
2017-08-11 23:55:37 +0430

    you are going to work in know

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,7 +3,8 @@ var mongoose = require('mongoose');
 // there is the history why they do this ... word
 mongoose.Promise = global.Promise;
 // mongodb://<dbuser>:<dbpassword>@ds055895.mlab.com:55895/danielapp
-mongoose.connect( 'mongodb://daniel:%$#@!daniel1997@ds055895.mlab.com:55895/danielapp' ||'mongodb://localhost:27017/TodoApp', {
+var databaseuri = process.env.DATABASEURI;
+mongoose.connect( databaseuri ||'mongodb://localhost:27017/TodoApp', {
     useMongoClient: true
 }); 
 
